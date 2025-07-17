8edb7e52d3ec3427265f3ab6522879bff1570cfc
GFAUnDead <contact@gfaundead.stream>
2025-07-18 00:45:30 +1000

    Disable contact form and add maintenance notice
    The contact form fields and submit button are now disabled, and a notification has been added to inform users that the form is under maintenance. Users are directed to contact via email instead.

--- contact.php
+++ contact.php
@@ -5,27 +5,32 @@ ob_start();
 ?>
 <div class="content">
     <h2>Contact Us</h2>
-    <form action="#" method="post">
+    <form>
+        <div class="notification is-info">
+            <strong>Notice:</strong> Our online contact form is currently undergoing scheduled maintenance to ensure the highest level of service and security for our clients.<br>
+            For all inquiries, please email us directly at <a href="mailto:talant@aetia.com.au">talant@aetia.com.au</a>.<br>
+            We appreciate your understanding and look forward to assisting you.
+        </div>
         <div class="field">
             <label class="label">Name</label>
             <div class="control">
-                <input class="input" type="text" name="name" required>
+                <input class="input" type="text" name="name" disabled>
             </div>
         </div>
         <div class="field">
             <label class="label">Email</label>
             <div class="control">
-                <input class="input" type="email" name="email" required>
+                <input class="input" type="email" name="email" disabled>
             </div>
         </div>
         <div class="field">
             <label class="label">Message</label>
             <div class="control">
-                <textarea class="textarea" name="message" required></textarea>
+                <textarea class="textarea" name="message" disabled></textarea>
             </div>
         </div>
         <div class="control">
-            <button class="button is-link" type="submit">Send</button>
+            <button class="button is-link" type="submit" disabled>Send</button>
         </div>
     </form>
     <a class="button is-light" href="index.php">Back to Home</a>
