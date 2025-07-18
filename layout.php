b23b3a2004725900c6c14d6ae37d301548a781c9
GFAUnDead <contact@gfaundead.stream>
2025-07-18 21:20:23 +1000

    Add footer and update Bulma version in layout.php
    Introduced a new footer with contact information and copyright notice. Updated Bulma CSS to version 1.0.4 and added Font Awesome for icon support.

--- layout.php
+++ layout.php
@@ -10,7 +10,7 @@ if (!isset($content)) $content = '';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars($pageTitle) ?></title>
     <link rel="icon" type="image/x-icon" href="img/logo.ico">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.0/css/bulma.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
 </head>
 <body>
     <!-- Navigation Bar -->
@@ -39,6 +39,20 @@ if (!isset($content)) $content = '';
             <?= $content ?>
         </div>
     </section>
+    <footer class="footer has-background-light mt-6">
+      <div class="content has-text-centered">
+        <p>
+          <img src="img/logo.png" alt="Aetia Logo" style="max-height:2rem;vertical-align:middle;"> <strong>Aetia Talant Agency</strong><br>
+          <span class="icon-text">
+            <span class="icon"><i class="fas fa-envelope"></i></span>
+            <span><a href="mailto:talant@aetia.com.au">talant@aetia.com.au</a></span>
+          </span>
+        </p>
+        <p class="is-size-7">&copy; <?= date('Y') ?> Aetia Talant Agency. All rights reserved.</p>
+      </div>
+    </footer>
     <script src="js/navbar.js"></script>
+    <!-- Font Awesome for icons -->
+    <script src="https://kit.fontawesome.com/7b8e1e2e2a.js" crossorigin="anonymous"></script>
 </body>
 </html>
